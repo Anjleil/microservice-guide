09242a820f2dacdba674071986e9e929afb0d6da
Anjleil <timofandr@gmail.com>
2025-01-26 17:12:58 +0300

    initial commit

---
+++ src/main/java/com/projects/learning/microserviceguide/MicroserviceGuideApplication.java
@@ -0,0 +1,13 @@
+package com.projects.learning.microserviceguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/learning/microserviceguide/MicroserviceGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.learning.microserviceguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
